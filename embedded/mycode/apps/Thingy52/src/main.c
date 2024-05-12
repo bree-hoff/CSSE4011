1bcc14aee0b5743a4a5ad5e578296324f76c3be7
Lewis Young <lewis.young@student.uq.edu.au>
2024-05-12 14:09:59 +1000

    ble button press working with eddystone

--- embedded/mycode/apps/Thingy52/src/main.c
+++ embedded/mycode/apps/Thingy52/src/main.c
@@ -1,9 +1,11 @@
-/*
- * Copyright (c) 2016 Open-RnD Sp. z o.o.
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+/**
+ ******************************************************************************
+ * @file
+ * @author  Lewis Young
+ * @date    12/05/2024
+ * @brief
+ ******************************************************************************
+ **/
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
@@ -11,126 +13,116 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
-
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/uuid.h>
 
-#define SLEEP_TIME_MS	1
-#define DEBOUNCE_DELAY_MS 50
-
-/*
- * iBeacon Stuff
- */
-#define ADV_PARAM                                                              \
-  BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_IDENTITY | BT_LE_ADV_OPT_USE_NAME, BT_GAP_ADV_FAST_INT_MIN_1,       \
+#define ADV_PARAM                                                                                  \
+  BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_IDENTITY | BT_LE_ADV_OPT_USE_NAME, BT_GAP_ADV_FAST_INT_MIN_1,  \
                   BT_GAP_ADV_FAST_INT_MAX_1, NULL)
 
-static uint8_t iBeaconData[] = {0x4c, 0x00,           /* Apple */
-                                0x02, 0x15,           /* iBeacon */
-                                0x00,  0x00,  0x00, 0x00, /* UUID[15..12] */
-                                0x00,  0x00,            /* UUID[11..10] */
-                                0x00,  0x00,            /* UUID[9..8] */
-                                0x00,  0x00,            /* UUID[7..6] */
-                                0x00,  0x00,  0x00, 0x00, 0x00, 0x00, /* UUID[5..0] */
-                                0x00, 0x00,                     /* Major */
-                                0x00, 0x00,                     /* Minor */
-                                0xc8}; /* Calibrated RSSI @ 1m -56dBm*/
+static uint8_t data[] = {0xaa, 0xfe, /* Eddystone UUID */
+                         0x00,       /* Eddystone-UID frame type */
+                         0x00,       /* Calibrated Tx power at 0m */
+                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                         0x00, 0x00, 0x00, 0x00,              /* 10-byte Namespace */
+                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; /* 6-byte Instance */
 
-static struct bt_data ad[] = {
-    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    BT_DATA(BT_DATA_MANUFACTURER_DATA, iBeaconData, 25)};
+static const struct bt_data ad[] = {BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+                                    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
+                                    BT_DATA(BT_DATA_MANUFACTURER_DATA, data, 20)};
 
 
+#define SLEEP_TIME_MS 1
+#define DEBOUNCE_TIME 220
+
+static uint32_t prev_tick;
+static uint8_t buttonPressFlag;
 /*
- * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ *
  */
-#define SW0_NODE	DT_ALIAS(sw0)
-#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
-#error "Unsupported board: sw0 devicetree alias is not defined"
-#endif
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
+#define SW0_NODE DT_ALIAS(sw0)
+#define LED_NODE DT_ALIAS(led2)
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
 static struct gpio_callback button_cb_data;
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
-/*
- * The led0 devicetree alias is optional. If present, we'll use it
- * to turn on the LED whenever the button is pressed.
- */
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-						     {0});
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
 
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
-{
-	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
-	bt_le_adv_stop();
+  // Check that Button has not being recently pressed (debounce logic)
+  if (k_uptime_get_32() - prev_tick > DEBOUNCE_TIME) {
+    printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	buttonPressFlag = 1;
+  }
 
+  // Update Time
+  prev_tick = k_uptime_get_32();
 }
 
-int main(void)
-{
-	int ret;
-	// Start advertising
-	bt_enable(NULL);
-	
-	
+int main(void) {
+  int ret;
 
-	if (!gpio_is_ready_dt(&button)) {
-		printk("Error: button device %s is not ready\n",
-		       button.port->name);
-		return 0;
-	}
+  if (!gpio_is_ready_dt(&button)) {
+    printk("Error: button device %s is not ready\n", button.port->name);
+    return 0;
+  }
 
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button.port->name, button.pin);
-		return 0;
-	}
+  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+  if (ret != 0) {
+    printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name, button.pin);
+    return 0;
+  }
 
-	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_TO_ACTIVE);
-	if (ret != 0) {
-		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
-		return 0;
-	}
+  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+  if (ret != 0) {
+    printk("Error %d: failed to configure interrupt on %s pin %d\n", ret, button.port->name,
+           button.pin);
+    return 0;
+  }
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
-	gpio_add_callback(button.port, &button_cb_data);
-	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+  gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+  gpio_add_callback(button.port, &button_cb_data);
 
-	if (led.port && !gpio_is_ready_dt(&led)) {
-		printk("Error %d: LED device %s is not ready; ignoring it\n",
-		       ret, led.port->name);
-		led.port = NULL;
-	}
-	if (led.port) {
-		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-		if (ret != 0) {
-			printk("Error %d: failed to configure LED device %s pin %d\n",
-			       ret, led.port->name, led.pin);
-			led.port = NULL;
+  if (!gpio_is_ready_dt(&led)) {
+    return 0;
+  }
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    return 0;
+  }
+
+  ret = bt_enable(NULL);
+  if (ret) {
+    printk("Bluetooth init failed (err %d)\n", ret);
+  }
+  ret = bt_le_adv_start(ADV_PARAM, ad, ARRAY_SIZE(ad), NULL, 0);
+  if (ret) {
+    printk("Advertising failed to start (err %d)\n", ret);
+    return 0;
+  }
+
+  gpio_pin_set_dt(&led, true);
+
+  while (1) {
+
+	if(buttonPressFlag == 1) {
+		
+		buttonPressFlag = 0;
+
+		if(data[19] == 0x01) {
+
+			data[19] = 0x00;
 		} else {
-			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
+			data[19] = 0x01;
 		}
+
+		bt_le_adv_update_data(ad, ARRAY_SIZE(ad), NULL, 0);
 	}
 
-	printk("Press the button\n");
-	bt_le_adv_start(ADV_PARAM, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (led.port) {
-		while (1) {
-			/* If we have an LED, match its state to the button's. */
-			int val = gpio_pin_get_dt(&button);
+    k_msleep(SLEEP_TIME_MS);
+  }
 
-			if (val >= 0) {
-				gpio_pin_set_dt(&led, val);
-				
-			}
-			k_msleep(SLEEP_TIME_MS);
-		}
-	}
-	return 0;
+  return 0;
 }
