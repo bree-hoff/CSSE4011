96f33bf5cb1d88d6bec892e02072fb84bc92f3e5
brianna hoffman <s4698661@student.uq.edu.au>
2024-05-19 18:07:49 +1000

    update for improved model

--- pc/BH/ml_gesture.py
+++ pc/BH/ml_gesture.py
@@ -14,19 +14,24 @@ from fastai.vision.all import *
 
 types = 'call', 'dislike','fist','four','like','mute','ok','one','palm','peace', 'peace_inverted', 'rock', 'stop', 'stop_inverted', 'three', 'three2', 'two_up', 'two_up_inverted', 'relaxed'
 
-
+set_seed(42)
 path = '/Users/brianna/Documents/2024/Semester 1/CSSE4011/Project/project_repo/CSSE4011/training_images/'
-dls = DataBlock(
+
+db = DataBlock(
     blocks=(ImageBlock, CategoryBlock), 
     get_items=get_image_files, 
     splitter=RandomSplitter(valid_pct=0.2, seed=42),
     get_y=parent_label,
-    item_tfms=[Resize(192, method='squish')]
-).dataloaders(path, bs=32)
+    item_tfms=[Resize(128, method='squish')],
+)
+
+tfms = aug_transforms()
+dls = db.dataloaders(path, path=path, bs=64, batch_tfms=[*tfms])
+ 
 
 dls.show_batch(max_n=6)
 
 learn = vision_learner(dls, resnet18, metrics=error_rate)
-learn.fine_tune(3)
+learn.fine_tune(5, 0.025)
 
 learn.export(fname="different_export.pkl")
